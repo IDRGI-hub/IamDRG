a612eeb63cc440d3102b8032f12111c306341a0d
IDRGI-hub <Danila17102002@gmail.com>
2025-07-06 03:22:24 +0300

    v1/2

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-templates = Jinja2Templates(directory="app/templates")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
